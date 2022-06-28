92ae2fa6521b0c263f8bad064b0ab5d487674b0e
pogrebmark <100341790+pogrebmark@users.noreply.github.com>
2022-06-28 09:48:33 +0300

    Calcs
    2 calcs

---
+++ Calculator2/Calculator2/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double firstvalue, twovalue;
+            string operatorV;
+
+            Console.WriteLine("Введите первое значение");
+            firstvalue = Double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите второе значение");
+            twovalue = Double.Parse(Console.ReadLine());
+
+            Console.WriteLine("рите метод операции + | - | * | /");
+            operatorV = Console.ReadLine();
+
+            switch (operatorV)
+            {
+                case "+":
+                    Console.WriteLine(firstvalue + twovalue);
+                    break;
+
+                case "-":
+                    Console.WriteLine(firstvalue - twovalue);
+                    break;
+
+                case "*":
+                    Console.WriteLine(firstvalue * twovalue);
+                    break;
+
+                case "/":
+                    if (firstvalue == 0)
+                        Console.WriteLine(0);
+                    else if (twovalue == 0)
+                        Console.WriteLine(0);
+                    else
+                        Console.WriteLine(firstvalue / twovalue);
+                    break;
+
+                default:
+                    Console.WriteLine("Неизвестная комманда, повотрите попытку!");
+                    break;
+            }
+            Console.ReadKey();
+
+
+        }
+    }
+}
---
+++ HelloWorldAPPWF/HelloWorldAPPWF/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HelloWorldAPPWF
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Start1_Click(object sender, RoutedEventArgs e)
+        {
+            string a = TextBox1.Text;
+            string b = TextBox2.Text;
+            int a_1 = Convert.ToInt32(a);
+            int b_1 = Convert.ToInt32(b);
+            int ab = a_1 + b_1;
+            TextBox3.Text = ab.ToString();
+        }
+    }
+}
